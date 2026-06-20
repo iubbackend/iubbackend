11d4aca31b5beb553b818eab597e06af73f2e8a0
iubbackend <iubbackend@gmail.com>
2026-06-20 23:54:03 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import PWARegistration from "@/components/PWARegistration";
+import PWARegistration from "../components/PWARegistration";
 
 const inter = Inter({ subsets: ["latin"] });
 
